90c7ccb7527bdfa125aca037cf2dcef4974d3784
Jess <71243378+JessToni@users.noreply.github.com>
2026-01-23 17:01:16 +0800

    feat: add header and guest playback controls to CreateRoomPage
    - Add "Create a Room" title typography
    - Implement RadioGrup for guest playback state (Play/Pause vs No Control)
    - Layout structure using Material UI Grid

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -11,11 +11,41 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel'
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <p>This is the create room page</p>
+        return (
+        <Grid container spacing={1}>
+            <Grid item xs={12} align='center'>
+                <Typography component='h4' variant='h4'>
+                    Create A Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <FormControl component='fieldset'>
+                    <FormHelperText>
+                        <div align='center'>Guest Control of Playback State</div>
+                    </FormHelperText>
+                    <RadioGroup row defaultValue='true'>
+                        <FormControlLabel 
+                        value='true' 
+                        control={<Radio color='primary'/>}
+                        label='Play/Pause'
+                        labelPlacement='bottom'
+                        />
+                        <FormControlLabel 
+                        value='false' 
+                        control={<Radio color='secondary'/>}
+                        label='No Control'
+                        labelPlacement='bottom'
+                        />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+        </Grid>
+        );
     }
 }
