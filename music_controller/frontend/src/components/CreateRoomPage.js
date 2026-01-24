9fca83c02b445ba7b996debe657954da74997038
Jess <71243378+JessToni@users.noreply.github.com>
2026-01-24 23:59:42 +0800

    feat: change grid container direction to column and add new grid items - Add new FormControl for "Votes required to skip song" - Add "Create a Room" and "Back" buttons

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -18,7 +18,7 @@ export default class CreateRoomPage extends Component {
 
     render() {
         return (
-        <Grid container spacing={1}>
+        <Grid container spacing={1} direction='column'>
             <Grid item xs={12} align='center'>
                 <Typography component='h4' variant='h4'>
                     Create A Room
@@ -44,6 +44,36 @@ export default class CreateRoomPage extends Component {
                         />
                     </RadioGroup>
                 </FormControl>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <FormControl>
+                    <TextField 
+                    required={true} 
+                    type='number' 
+                    defaultValue={this.defaultVotes} 
+                    slotProps={{
+                        htmlInput: {
+                            min: 1,
+                            style: {textAlign: 'center'},
+                        }
+                    }}
+                    />
+                    <FormHelperText>
+                        <div align='center'>
+                            Votes required to skip song
+                        </div>
+                    </FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Button color='primary' variant='contained'>
+                    Create A Room
+                </Button>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Button color='secondary' variant='contained' to='/' component={Link}>
+                    Back
+                </Button>
             </Grid>
         </Grid>
         );
