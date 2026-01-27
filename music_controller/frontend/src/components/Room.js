2540c841c5a55db7d942ff132df1e5325203a919
Jess <71243378+JessToni@users.noreply.github.com>
2026-01-27 22:22:56 +0800

    feat: implement room details fetching from API - Add useEffect hook to fetch room data using roomCode parameter. - Update state with data retrieved from /api/get-room. - Mapped backend response fields to component state.

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { data, useParams } from 'react-router-dom';
 
 const Room = () => {
 
@@ -10,6 +10,18 @@ const Room = () => {
         guestCanPause: false,
         isHost: false,
     });
+
+    useEffect(() => {
+        fetch('/api/get-room' + '?code=' + roomCode)
+            .then((response) => response.json())
+            .then((data) => {
+                setState({
+                    votesToSkip: data.votes_to_skip,
+                    guestCanPause: data.guest_can_pause,
+                    isHost: data.is_host,
+                })
+            });
+    }, [roomCode]);
 
     return (
         <div>
