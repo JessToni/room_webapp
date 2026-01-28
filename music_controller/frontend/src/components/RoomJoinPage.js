ebed853b6986d8feba3f4216a131806dde5ce14c
Jess <71243378+JessToni@users.noreply.github.com>
2026-01-28 17:45:24 +0800

    feat: implement handleTextField change for RoomJoinPage.js - Set to capture changes in roomCode - Bind method to RoomJoinPage class

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import { TextField,Button, Grid, Typography } from "@mui/material"
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: '',
+            error: 'hello',
+        }
+        this._handleTextFieldChange = this._handleTextFieldChange.bind(this);
     }
 
     render() {
-        return <p>This is the room join page</p>
+        return (
+            <Grid container spacing={1} align='center' direction='column'>
+                <Grid item xs={12}>
+                    <Typography variant='h4' component='h4'>
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                    error={this.state.error}
+                    label='Code'
+                    placeholder='Enter a Room Code'
+                    value={this.state.roomCode}
+                    helperText={this.state.error}
+                    variant='outlined'
+                    onChange={this._handleTextFieldChange}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant='contained' color='primary' to='/' onClick>
+                    Enter Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant='contained' color='secondary' to='/' component={Link}>
+                    Back
+                    </Button>
+                </Grid>
+            </Grid>
+        )
+    }
+
+    _handleTextFieldChange(e) {
+        this.setState({
+            roomCode: e.target.value
+        })
     }
 }
